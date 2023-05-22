4eb03388e92b04d66dab39b90d6b3dff548918ce
Preffet <jstmtr@gmail.com>
2023-05-23 00:33:56 +0100

    updated mqtt

--- real_implementation/GatewayMQTT.py
+++ real_implementation/GatewayMQTT.py
@@ -1,20 +1,136 @@
 import json
 import base64
+import os
 import paho.mqtt.client as mqtt
 from datetime import datetime
-
-# Type the IP of your server
+import pandas as pd
+from numpy import where
+from sklearn.svm import OneClassSVM
+# server information
 mqttip = '192.168.230.1'
 mqttport = 1883
+model = None
+max_df_value = None
 
-# Type your username and password. If the private MQTT server does not require username and password, commend the lines
+
+# ANSI escape codes to print coloured/bold text
+class colours:
+    ENDC = '\033[0m'
+    CYAN = '\033[96m'
+    BOLD = '\033[1m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
+    BLUE = '\033[94m'
+    ORANGE = '\033[38;5;173m'
+
+
+# Type your username and password. If the private MQTT server does not require username and password, comment the lines
 # mqtt_username = 'username'
 # mqtt_password = 'password'
 
 # Replace the "id" with the id of your application and "eui" with your device eui
 # mqttrxtopic = 'application/SensorBox/device/a8610a3439307f10/rx'
-
 mqttrxtopic = 'application/SensorBox/device/a8610a3530408b09/rx'
+mqttc = mqtt.Client()
+
+
+def svm_model():
+    # get the path to the training data
+    path_to_data = os.path.dirname(os.path.dirname(__file__))
+    path_to_data = path_to_data + "/datasets/training_light_data.csv"
+    # import training data
+    data = pd.read_csv(path_to_data)
+    # choose the features
+    df = data[["hour", "light"]]
+    # normalise the data
+    normalized_training_data = df / df.max()
+
+    # model specification
+    model = OneClassSVM(kernel='rbf', gamma=39, nu=0.03)
+    model.fit(normalized_training_data)
+    # max df value (to be used for data normalisation)
+    max_df_value = df.max()
+    return model, max_df_value
+
+
+def anomaly_detection(lux_value, device_name, dev_eui):
+    # round lux values
+    lux_value_for_prediction = round(lux_value)
+    current_date_and_time = datetime.now()
+    hour = current_date_and_time.hour
+    # minute = currentDateAndTime.minute
+    # timestamp_in_minutes = hour*60+minute
+    # print(minute)
+    # convert the data to a list
+    data_list = [hour, lux_value_for_prediction]
+    # convert data to a dataframe
+    data_to_be_predicted = pd.DataFrame([data_list], columns=["hour", "light"])
+    # normalise the data
+    normalized_data_for_predictions = data_to_be_predicted / max_df_value
+
+    # make the prediction
+    prediction = model.predict(normalized_data_for_predictions)
+
+    # filter outlier indexes
+    outlier_index = where(prediction == -1)
+    # filter outlier values
+    outlier_values = normalized_data_for_predictions.iloc[outlier_index]
+    if not outlier_values.size == 0:
+        in_the_blocklist_already = False
+        decision = "anomalous"
+        # print that the received data is anomalous
+        # decision: anomalous
+        print(f"\n{colours.BOLD}{colours.RED}⫸{colours.ENDC}"
+              f" SVM Decision: {colours.BOLD}{colours.RED}{decision}{colours.ENDC}")
+
+        # get the path to the blocklist.csv file
+        path_to_blocklist_csv = os.path.dirname(__file__)
+        path_to_blocklist_csv = path_to_blocklist_csv + "/detection_system_files/blocklist.csv"
+        # add the node to the blocklist
+        with open(path_to_blocklist_csv, "a+") as blocklist:
+            # check if the node is not in the blocklist already
+            blocklist.seek(0)
+            blocklist_contents = blocklist.read()
+            if device_name not in blocklist_contents:
+                blocklist.write(f"{device_name},"
+                                f"{dev_eui},"
+                                f"{current_date_and_time.date()} "
+                                f"{current_date_and_time.hour}:{current_date_and_time.minute}\n")
+            else:
+                in_the_blocklist_already = True
+
+        # get the path to the malicious_data_received.csv file (logs)
+        path_to_logs = os.path.dirname(__file__)
+        path_to_logs = path_to_logs + "/detection_system_files/malicious_data_received.csv"
+        # add the malicious information received to the logs
+        with open(path_to_logs, "a+") as log_file:
+            log_file.write(f"{device_name},"
+                            f"{dev_eui},"
+                            f"{current_date_and_time.date()} "
+                            f"{current_date_and_time.hour}:{current_date_and_time.minute},"
+                           f"{lux_value_for_prediction}\n")
+
+        # if the device is not in the blocklist already
+        # Print: device added to the blocklist {device_name}, device EUI {device_eui}
+        if not in_the_blocklist_already:
+            print(f"{colours.BOLD}{colours.RED}⫸{colours.ENDC}"
+                  f" Device {colours.BOLD}"
+                  f"{colours.RED}{device_name}"
+                  f"{colours.ENDC} added to the blocklist{colours.ENDC}\n")
+        # if the device in the blocklist already print a reminder to inform the system administrators
+        else:
+            print(f"{colours.BOLD}{colours.RED}⫸ {colours.ENDC}"
+                  f"Device {colours.RED}{colours.BOLD}{device_name}{colours.ENDC} "
+                  f"is already in the blocklist,\n"
+                  f"   please run the{colours.RED}{colours.BOLD} send_email_report.py{colours.ENDC} script"
+                  f" to inform the administrators.\n")
+    else:
+        decision = "valid"
+        # print that the received data is valid
+        # decision: valid
+        print(f"\n{colours.BOLD}{colours.GREEN}⫸{colours.ENDC}"
+              f" Decision: {colours.BOLD}{colours.GREEN}{decision}{colours.ENDC}")
 
 
 # Convert string to hexadecimal
@@ -22,7 +138,7 @@ def strtohex(s):
     return r"\x" + r'\x'.join([hex(ord(c)).replace('0x', '') for c in s])
 
 
-# #Once subscribed to the message, call back this method
+# Once subscribed to the message, call back this method
 def onmessage(mqttc, obj, msg):
     onprintraknodeinfo(msg.payload)
 
@@ -39,10 +155,8 @@ def onprintnoderxinfo(jsonrx):
         fPort = jsonrx['fPort']
         data = jsonrx['data']
 
-        # datanumbers = base64.b64decode(data)
 
         datanumbers = bytearray(base64.b64decode(data))
-        print("DATANUMBERS: ", datanumbers)
         recv_value = int.from_bytes(datanumbers, byteorder='little')
 
         Soil_Temp_Nums = datanumbers[0:4]
@@ -78,6 +192,7 @@ def onprintnoderxinfo(jsonrx):
         BAT_Nums = datanumbers[26:28]
         BAT = int.from_bytes(BAT_Nums, byteorder='little')
 
+        '''
         print('Datanums: ', datanumbers)
         print('soil temp datanumbers: ', Soil_Temp_Nums)
         print('soil moist1 datanumbers: ', Soil_Moist1_Nums)
@@ -90,9 +205,7 @@ def onprintnoderxinfo(jsonrx):
         print('Amb T datanumbers: ', AmbT_Nums)
         print('Amb H datanumbers: ', AmbH_Nums)
         print('Battery Nums: ', BAT_Nums)
-
         print(recv_value)
-
         print("Soil Temp", undo_numpad(Soil_Temp))
         print("Soil Moist1", Soil_Moist1)
         print("Soil Moist2", Soil_Moist2)
@@ -104,25 +217,53 @@ def onprintnoderxinfo(jsonrx):
         print("Ambient Temp degC", undo_numpad(AmbT))
         print("Ambient Humidity", undo_humpad(AmbH))
         print("Battery", phnpk_bat_unpad(BAT))
-
-        print('Number of Bytes: ', len(datanumbers))
+        '''
+        # print('Number of Bytes: ', len(datanumbers))
 
         # Convert the timestamp to local time
         strlocaltime = datetime.fromtimestamp(timestamp)
-        print('---------------- devEUI:[%s] rxpk info -------------------' % devEUI)
-        print('+\t applicationName:\t%s' % applicationName)
-        print('+\t applicationID:\t\t%s' % applicationID)
-        print('+\t deviceName:\t\t%s' % deviceName)
-        print('+\t datetime:\t\t%s' % strlocaltime)
-        print('+\t fCnt:\t\t\t%d' % fCnt)
-        print('+\t fPort:\t\t\t%d' % fPort)
-        print('+\t data:\t\t\t%s' % data)
-        print('+\t data:\t\t\t%s' % datanumbers)
-        # print('+\t data:\t\t\t%s' % mydata)
-        # print('Data Length: ', len(mydata))
 
-        # print('+\t datahex:\t\t%s' % datahex)
-        print('----------------------------------------------------------')
+        # Print the device EUI and the header
+        print(f"\n\n\n{colours.CYAN}----------{colours.BLUE}"
+              f" devEUI:[{devEUI}] rxpk info{colours.CYAN} -----------{colours.ENDC}")
+        print(f"{colours.CYAN}+{colours.ENDC}\t deviceName:\t\t{deviceName}")
+        print(f"{colours.CYAN}+{colours.ENDC}\t datetime:\t\t{strlocaltime}")
+        print(f"{colours.CYAN}+{colours.ENDC}\t fPort:\t\t\t{fPort}")
+
+
+        # Print a text separator line
+        print(f"{colours.CYAN}---------------{colours.BLUE}----------------------------"
+              f"{colours.CYAN}---------------{colours.ENDC}\n")
+        # Print lux data numbers
+        print(f"{colours.BOLD}{colours.YELLOW}✦ {colours.ENDC}"
+              f"Lux data numbers: {colours.YELLOW}{colours.BOLD}"
+              f"{LUX_Nums}{colours.ENDC}")
+        # Print the actual Lux value
+        print(f"{colours.BOLD}{colours.YELLOW}✦ {colours.ENDC}"
+              f"Lux value: {colours.YELLOW}{colours.BOLD}"
+              f"{lux_unpad(LUX)}{colours.ENDC}")
+        # Print time value
+        current_date_and_time = datetime.now()
+        print(f"{colours.BOLD}{colours.YELLOW}✦ {colours.ENDC}"
+              f"Time value: {colours.YELLOW}{colours.BOLD}"
+              f"{str(current_date_and_time.hour)}:{str(current_date_and_time.minute)}{colours.ENDC}")
+        # Print timespan value
+        print(f"{colours.BOLD}{colours.YELLOW}✦ {colours.ENDC}"
+              f"Timespan value: {colours.YELLOW}{colours.BOLD}"
+              f"{str(current_date_and_time.hour*60+current_date_and_time.minute)}")
+
+        # Get the path to the clients.csv file
+        path_to_clients_csv = os.path.dirname(__file__)
+        path_to_clients_csv = path_to_clients_csv + "/detection_system_files/clients.csv"
+        # Add the device to the clients list
+        with open(path_to_clients_csv, "a+") as clients:
+            clients.seek(0)
+            if not (f"{deviceName},{devEUI}") in clients.read():
+                id = len(clients.readlines()) + 1
+                clients.write(f"{id},"f"{deviceName},"f"{devEUI}\n")
+
+        # Do anomaly detection
+        anomaly_detection(lux_unpad(LUX), deviceName, devEUI)
     except Exception as e:
         print(e)
     finally:
@@ -143,8 +284,6 @@ def onprintraknodeinfo(payload):
         tx_msg = '{"confirmed":true,"fPort":10,"data":"%s" }' % base64.b64encode(str_hello.encode("utf-8")).decode(
             "utf-8")
         mqttc.publish(tx_topic, tx_msg, qos=0, retain=False)
-        print('Send \'Hello RAKwireless\' to node %s' % deveui)
-
     except Exception as e:
         raise e
     finally:
@@ -178,14 +317,32 @@ def undo_humpad(num):
     return num
 
 
-mqttc = mqtt.Client()
-mqttc.on_message = onmessage
+# program entry point
+def main():
 
-# If there is no username and password, please comment the following line:
-# mqttc.usernamepwset(mqttusername, password=mqttpassword)
+    # train and assign the svm model
+    global model
+    model = svm_model()[0]
 
-# Connect to mqtt broker, the heartbeat time is 60s
-mqttc.connect(mqttip, mqttport, 60)
-mqttc.subscribe(mqttrxtopic, 0)
+    # assign the max dataframe values (will be used for data normalisation)
+    global max_df_value
+    max_df_value = svm_model()[1]
 
-mqttc.loop_forever()
+    # receive the message, print it and do anomaly detection
+    # function calls:
+    # onmessage() -> onprintraknodeinfo()
+    # -> onprintnoderxinfo() -> anomaly_detection()
+    # if data is anomalous send_email()
+    mqttc.on_message = onmessage
+
+    # If there was username and password:
+    # mqttc.usernamepwset(mqttusername, password=mqttpassword)
+
+    # Connect to mqtt broker, the heartbeat time is 60s
+    mqttc.connect(mqttip, mqttport, 60)
+    mqttc.subscribe(mqttrxtopic, 0)
+    mqttc.loop_forever()
+
+
+if __name__ == "__main__":
+    main()
